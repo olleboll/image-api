328d90ed218991d5cb9cf037eb11513ce753821e
Olle Wahlström <owlen88@gmail.com>
2022-12-16 10:15:09 +0100

    Create image cutout function

--- img/image.go
+++ img/image.go
@@ -1,12 +1,12 @@
 package img
 
 import (
-	"fmt"
 	"image"
-	_ "image/gif"
-	_ "image/png"
-	_ "image/jpeg"
+	gif "image/gif"
+	png "image/png"
+	jpeg "image/jpeg"
 	"encoding/base64"
+	"bytes"
 	"strings"
 	"time"
 )
@@ -25,14 +25,22 @@ type Image struct {
 	Metadata Metadata  `json:"metadata,omitempty"`
 }
 
-func GenerateImageData(data []byte) (*Image, error) {
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(string(data)))
-	
-	imageObj, format, err := image.Decode(reader)
+type Cutout struct {
+	X int
+	Y int
+	W int
+	H int
+}
+
+func GenerateImageData(data *[]byte) (*Image, error) {
+	// Create image.Image instace
+	imageObj, format, err := getImageData(data)
 	if (err != nil) {
 		// Return specific error for 422?
 		return nil, err
 	}
+
+	// Populate metadata
 	bounds := imageObj.Bounds()
 	imageMetadata := Metadata{
 		Width: bounds.Dx(),
@@ -42,15 +50,40 @@ func GenerateImageData(data []byte) (*Image, error) {
 	}
 	imageMetadata.Size = imageMetadata.Width * imageMetadata.Height * 4 / 1024
 
+	// Create Image struct
 	_image := Image{
-		Data: string(data),
+		Data: string(*data),
 		Metadata: imageMetadata,
 	}
 
 	return &_image, nil
 }
 
-func GetImageCutout(img *Image) (*Image, error) {
-	fmt.Println("Getting cutout")
-	return &Image{}, nil
+func GetImageCutout(data *[]byte, cutout Cutout) ([]byte, error) {
+	// Get original image data
+	_image, format, err := getImageData(data)
+
+	// Create subimage
+	imageObj := _image.(interface {
+        SubImage(r image.Rectangle) image.Image
+    }).SubImage(image.Rect(cutout.X, cutout.Y, cutout.W, cutout.H))
+
+	// Depending on format, use the correct Encoder
+	buf := new(bytes.Buffer)
+	switch format {
+		case "jpeg":
+			err = jpeg.Encode(buf, imageObj, nil)
+		case "png":
+			err = png.Encode(buf, imageObj)
+		case "gif":
+			err = gif.Encode(buf, imageObj, nil)
+	}
+
+	return []byte(base64.StdEncoding.EncodeToString(buf.Bytes())), err
+	
+}
+
+func getImageData(data *[]byte) (image.Image, string, error){
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(string(*data)))
+	return image.Decode(reader)
 }
